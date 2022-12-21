d4c90debc130c3816631741558ccd7fd7475d895
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-12-21 15:13:32 +0530

    Update the main filefor linters

--- morse_code_decoding.rb
+++ morse_code_decoding.rb
@@ -1,34 +1,35 @@
 CODES = {
-  'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..',
-  'E' => '.', 'F' => '..-.', 'G' => '--.', 'H' => '....',
-  'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
-  'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.',
-  'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-',
-  'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-', 'Y' => '-.--', 'Z' => '--..'
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
 }.freeze
 
 def decode_char(morse_char)
   @char = CODES[morse_char]
-    @char
+  @char
 end
 
 def decode_word(morse_word)
   @word = ''
-    @array_of_words = morse_word.split  
-    @array_of_words.map do |i|
-      @word += decode_char(i)
-end
-  
-    "#{@word} "
-end
-  
-def decode(morse_code)
-    @coded_word = ''
-    @morse_code_arr = morse_code.split('   ')
-    @morse_code_arr.map do |morse_word|
-      @coded_word += decode_word(morse_word)
-end
-  
-    puts "#{@coded_word} "
+  @array_of_words = morse_word.split
+
+  @array_of_words.map do |i|
+    @word += decode_char(i)
   end
+
+  "#{@word} "
+end
+
+def decode(morse_code)
+  @final_word = ''
+  @morse_code_arr = morse_code.split('   ')
+  @morse_code_arr.map do |morse_word|
+    @final_word += decode_word(morse_word)
+  end
+
+  puts "#{@final_word} "
+end
+
 decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
