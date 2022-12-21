8a3062c29d12cb3ad6904c2e94b4d76a43ce1db7
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-12-21 15:04:43 +0530

    Put proper indentation

--- morse_code_decoding.rb
+++ morse_code_decoding.rb
@@ -8,29 +8,27 @@ CODES = {
 }.freeze
 
 def decode_char(morse_char)
-    @char = CODES[morse_char]
+  @char = CODES[morse_char]
     @char
-  end
-  
-  def decode_word(morse_word)
-    @word = ''
-    @array_of_words = morse_word.split
-  
+end
+
+def decode_word(morse_word)
+  @word = ''
+    @array_of_words = morse_word.split  
     @array_of_words.map do |i|
       @word += decode_char(i)
-    end
+end
   
     "#{@word} "
-  end
+end
   
-  def decode(morse_code)
+def decode(morse_code)
     @coded_word = ''
     @morse_code_arr = morse_code.split('   ')
     @morse_code_arr.map do |morse_word|
       @coded_word += decode_word(morse_word)
-    end
+end
   
     puts "#{@coded_word} "
   end
-  
-  decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
